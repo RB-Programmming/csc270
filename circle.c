0b25aa03c5271aab0494eaf016f4481e84c28e7d
Your Name <you@example.com>
2017-09-02 15:52:14 -0400

    Fixed several bugs.

--- circle.c
+++ circle.c
@@ -1,3 +1,4 @@
+
 /*
  * circle.c
  * Assignment#2
@@ -11,41 +12,6 @@
 #include <stdio.h>
 //Defines the value of PI (a preprocessor symbol)  as 3.1415926 for use throughout this program.
 #define PI 3.1415926
-
-/**
- *  The main method of the circle.c program requests that the user enters a positive number as a circle's radius
- *  If the entered value is positive, the program will then use the radius to calculate the diameter, circumference, and area of a circle with said radius, rounding to the third
- *  decimal place when needed. Otherwise, the program will return 1, indicating an incorrect value was used for the radius.
- */
-
-int main (void){
-	//Define and initialize a float variable to contain the radius entered by the user. 
-	float radius=0;
-	printf("Welcome to circle.c, a program for calculating a circle's  diameter, circumference, and area.\nPlease enter a positive number as the circle's radius.\n");
-	scanf("%f", &radius);
-	//Return 1 (indicating an error has occurred due to the invalid radius entry (it should not be less than 0) if the radius is less than zero.
-	if(radius < 0){
-		return 1;
-	}
-	//Initialize and define three float fields, calculating the value of each one based on the entered radius.
-	printf("Calculating...");
-	float diameter=0;
-	float circumference=0;
-	float area=0;
-
-	//Prints the calculated values of the circle with the entered radius in the following format.
-
-	/**
-	 * A circle with radius (radius):
-	 *	Diameter: (diameter)
-	 *	Circumference: (circumference)
-	 *	Area: (area)
-	 */
-	printf("A circle with radius %0.3f: \n\t Diameter: %0.3f \n\t Circumference %0.3f \n\t Area: %0.3f", radius, diameter, circumference, area);
-
-	return 0;
-	//Ends program.
-}
 
 /**
  * calcDiameter() takes a single float (float r, which should be the user entered radius) and multiplies it by 2 to find and return the diameter as a float.
@@ -73,4 +39,44 @@ float calcArea (float r){
 	float a=rSquared*PI;
 	return a;
 }
+
+/**
+ *  The main method of the circle.c program requests that the user enters a positive number as a circle's radius
+ *  If the entered value is positive, the program will then use the radius to calculate the diameter, circumference, and area of a circle with said radius, rounding to the third
+ *  decimal place when needed. Otherwise, the program will return 1, indicating an incorrect value was used for the radius.
+ */
+
+int main (void){
+	//Define and initialize a float variable to contain the radius entered by the user. 
+	float radius=0;
+	printf("Welcome to circle.c, a program for calculating a circle's  diameter, circumference, and area.\nPlease enter a positive number as the circle's radius.\n");
+	scanf("%f", &radius);
+	//Return 1 (indicating an error has occurred due to the invalid radius entry (it should not be less than 0) if the radius is less than zero.
+	if(radius < 0){
+		return 1;
+	}
+	//Initialize and define three float fields, calculating the value of each one based on the entered radius.
+	printf("Calculating...");
+	float diameter=0;
+	float circumference=0;
+	float area=0;
+
+	diameter=calcDiameter(radius);
+	circumference=calcCircumference(diameter);
+	area=calcArea(radius);
+
+	//Prints the calculated values of the circle with the entered radius in the following format.
+
+	/**
+	 * A circle with radius (radius):
+	 *	Diameter: (diameter)
+	 *	Circumference: (circumference)
+	 *	Area: (area)
+	 */
+	printf("A circle with radius %0.3f: \n\t Diameter: %0.3f \n\t Circumference %0.3f \n\t Area: %0.3f\n", radius, diameter, circumference, area);
+
+	return 0;
+	//Ends program.
+}
+
 
