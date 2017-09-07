286e68e3a6fb7412d5658afb3d4401a6870ad6f6
Your Name <you@example.com>
2017-09-07 15:51:14 -0400

    temp.c updated

--- temp.c
+++ temp.c
@@ -17,7 +17,8 @@
  */
 
 /**
- * celsiusToKelvin() converts a temperature in celsius (the float c parameter) to its equivalent value in kelvin and returns that equivalent value.
+ * celsiusToKelvin() converts a temperature in celsius (the float c parameter)
+ * to its equivalent value in kelvin and returns that equivalent value.
  * K=C+273.15
  */
 float celsiusToKelvin(float c){
@@ -26,7 +27,8 @@ float celsiusToKelvin(float c){
 }
 
 /**
- * kelvinToCelsius() converts a temperature in kelvin (the float k parameter) to its equivalent value in celsius, and returns that equivalent value.
+ * kelvinToCelsius() converts a temperature in kelvin (the float k parameter)
+ * to its equivalent value in celsius, and returns that equivalent value.
  * C=K-273.15
  */
 
@@ -36,7 +38,8 @@ float kelvinToCelsius(float k){
 }
 
 /**
- * celsiusToFahrenheit() converts a celsius temperature (the float c parameter) to its equivalent fahrenheit temperature, and returns that equivalent temperature.
+ * celsiusToFahrenheit() converts a celsius temperature (the float c parameter)
+ * to its equivalent fahrenheit temperature, and returns that equivalent temperature.
  * F=(C*1.8)+32
  */
 float celsiusToFahrenheit(float c){
@@ -45,7 +48,8 @@ float celsiusToFahrenheit(float c){
 }
 
 /**
- * fahrenheitToCelsius() converts a fahrenheit temperature (the float f parameter) to its equivalent value in celsius, then returns that equivalent value.
+ * fahrenheitToCelsius() converts a fahrenheit temperature (the float f parameter)
+ * to its equivalent value in celsius, then returns that equivalent value.
  * C=(F-32)/1.8
  */
 float fahrenheitToCelsius(float f){
@@ -54,7 +58,8 @@ float fahrenheitToCelsius(float f){
 }
 
 /**
- * kelvinToFahrenheit converts a value in kelvin to a value in fahrenheit by using the kelvinToCelsius() and celsiustoFahrenheit() method to convert from kelvin to celsius, and then celsius to fahrenheit. The value for fahrenheit is returned. 
+ * kelvinToFahrenheit converts a value in kelvin to a value in 
+ *fahrenheit by using the kelvinToCelsius() and celsiustoFahrenheit() method to convert from kelvin to celsius, and then celsius to fahrenheit. The value for fahrenheit is returned. 
  */
 float kelvinToFahrenheit(float k){
 	float cel=kelvinToCelsius(k); //Find the equivalent value of k in celsius.
@@ -69,9 +74,55 @@ float fahrenheitToKelvin(float f){
 	float kel=celsiusToKelvin(cel); //Get the equivalent value of f in kelvin.
 	return kel; 
 }
+
+
 /**
- * The main method used here...
+ * The main method used here reads and inputted temperature from the user,
+ * then displays the temperature and its equivalents in the other conversions.
+ * Example
+ * ./temp
+ * Enter input temperature: 18c
+ * 18.00 C converts to:
+ *	 64.40 F
+ *	 291.15 K
+ * As shown  here, the user inputs a number and a character. which is then
+ * read and defined as a temperature that can be converted. It is then
+ * converted to the different temperatures.
  */
 int main(void){
+	//Define and initialize field values.
+	//Values entered by the user.
+	float enteredTemp=0;
+	char enteredScale=' '; //Represent temperature scale (C, F, or K)
+	//Used to store the converted values of the original temperature.
+	float convertedTempA=0;
+	float convertedTempB=0;
 
+	//Read user input.
+	printf("Enter input temperature: ");
+	scanf("%f %c", &enteredTemp, &enteredScale); 
+
+	printf("\n%0.2f %c converts to:\n", enteredTemp, enteredScale);
+
+	//Check the scale/ If valid, make conversions and display them on the terminal.
+	if(enteredScale='f'  || enteredScale='F'){
+		convertedTempA=fahrenheitToCelsius(enteredTemp);
+		convertedTempB=fahrenheitToKelvin(enteredTemp);
+		printf("\t%0.2f C\n\t%0.2f K", convertedTempA, convertedTempB);
+	}
+	else if(enteredScale='c' || enteredScale='C'){
+		convertedTempA=celsiusToFahrenheit(enteredTemp);
+		convertedTempB=celsiusToKelvin(enteredTemp);
+		printf("\t%0.2f F\n\t%0.2f K", convertedTempA, convertedTempB);
+	}
+	else if(enteredScale='k' || enteredScale='K'){
+		convertedTempA=kelvinToFahnrenheit(enteredTemp);
+		convertedTempB=kelvinToCelsius(enteredTemp);
+		printf("\t%0.2f F\n\t%0.2f C", convertedTempA, convertedTempB);
+	|
+	else{
+		printf("Error: Invalid temperature scale entered.");
+		return 1;
+	}
+	return 0; //Once completed, return 0.
 }
